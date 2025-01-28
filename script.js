039784815e793983b7b2fe0071f9a117b4cd33fe
Devang Malhotra <119973585+devangmalhotra@users.noreply.github.com>
2025-01-28 14:49:12 -0500

    updated files

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let price = 1.87;
+let price = 1;
 let cid = [
   ['PENNY', 1.01],
   ['NICKEL', 2.05],
@@ -10,3 +10,17 @@ let cid = [
   ['TWENTY', 60],
   ['ONE HUNDRED', 100]
 ];
+let cash = document.getElementById("cash");
+
+const purchaseBtn = document.getElementById("purchase-btn");
+const changeDueContainer = document.getElementById("change-due");
+
+const calculate = () => {
+  if (cash.value < price) {
+    alert("Customer does not have enough money to purchase the item");
+  } else if (Number(cash.value) === price) {
+    changeDueContainer.innerHTML = "<p>No change due - customer paid with exact cash</p>"
+  }
+};
+
+purchaseBtn.addEventListener("click", calculate);
