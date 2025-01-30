b2d781b60604ff61f7574375c079a20441300956
Devang Malhotra <119973585+devangmalhotra@users.noreply.github.com>
2025-01-30 16:07:16 -0500

    updated js

--- script.js
+++ script.js
@@ -17,8 +17,8 @@ const purchaseBtn = document.getElementById("purchase-btn");
 const changeDueContainer = document.getElementById("change-due");
 let status = "";
 let change = 0;
-let coinValues = [100, 20, 10, 5, 1, 0.25, 0.1, 0.05, 0.01];
-let coinNames = ['ONE HUNDRED', 'TWENTY', 'TEN', 'FIVE', 'ONE', 'QUARTER', 'DIME', 'NICKEL', 'PENNY'];
+let coinValues = [0.01, 0.05, 0.1, 0.25, 1, 5, 10, 20, 100];
+let coinNames = ['PENNY', 'NICKEL', 'DIME', 'QUARTER', 'ONE', 'FIVE', 'TEN', 'TWENTY', 'ONE HUNDRED'];
 let amountWithEachCoin = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 
 
@@ -30,7 +30,9 @@ const ifCashBelowOrEqual = () => {
   } else {
     change = calculateChange();
     //console.log(change);
-    status = determineStatus(change);
+    let totalCid = getTotalCid(cid);
+    console.log(change);
+    status = determineStatus(change, totalCid);
     let dollarAmountOfEachCoin = coinsToGiveForChange(change);
     updatePage(status, dollarAmountOfEachCoin);
   }
@@ -40,7 +42,7 @@ const calculateChange = () => {
   return change = Number(cash.value) - price;
 }
 
-const determineStatus = (change) => {
+const getTotalCid = (cid) => {
   totalCid = 0;
   
   cid.forEach(i => {
@@ -49,6 +51,10 @@ const determineStatus = (change) => {
   
   totalCid = Number(totalCid.toFixed(2));
   
+  return totalCid;
+}
+
+const determineStatus = (change, totalCid) => {
   if (totalCid < change) {
     return "INSUFFICIENT_FUNDS";
   } else if (totalCid > change) {
@@ -61,15 +67,17 @@ const determineStatus = (change) => {
 const coinsToGiveForChange = (change) => {
   let dollarAmountOfEachCoin = [0, 0, 0, 0, 0, 0, 0, 0, 0];
   
-  let i = 0;
-  while(change > 0) {
-    if (coinValues[i] > change) {
-      i++;
+  let i = dollarAmountOfEachCoin.length - 1;
+  while(change > 0 && i >= 0) {
+    if (coinValues[i] > change || cid[i][1] === 0) {
+      i--;
       continue;
     } else {
       change -= coinValues[i];
       amountWithEachCoin[i] += 1;
       dollarAmountOfEachCoin[i] += coinValues[i];
+      cid[i][1] -= coinValues[i];
+      console.log(cid);
       //console.log(coinValues[i]);
       //console.log(amountWithEachCoin);
       //console.log(dollarAmountOfEachCoin);
@@ -92,7 +100,7 @@ const updatePage = (status, dollarAmountOfEachCoin) => {
   changeDueContainer.innerHTML += `<p>Status: ${status}</p>`;
   console.log(dollarAmountOfEachCoin)
   
-  for (let a = 0; a < dollarAmountOfEachCoin.length; a++){
+  for (let a = dollarAmountOfEachCoin.length - 1; a >= 0; a--){
     if (dollarAmountOfEachCoin[a] > 0) {
       changeDueContainer.innerHTML += `<p>${coinNames[a]}: $${dollarAmountOfEachCoin[a]}</p>`;
       console.log(`${coinNames[a]}: $${dollarAmountOfEachCoin[a]}`);
